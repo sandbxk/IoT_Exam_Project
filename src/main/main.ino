37206b2f97e899ad16ec17b7f18bcddcca43898e
Rasmus Sandbæk <sandbxk@gmail.com>
2023-03-06 15:21:34 +0100

    Burned it

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,15 +1,29 @@
 
-#define MICROWAVE_SENSOR_PIN 0
+#define MOTOR_1A 0
+#define MOTOR_2A 14
+
 
 // the setup routine runs once when you press reset:
 void setup() {
-  pinMode(MICROWAVE_SENSOR_PIN, INPUT_PULLDOWN);
+  pinMode(MOTOR_1A, OUTPUT);
+  pinMode(MOTOR_2A, OUTPUT);
   Serial.begin(115200);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  int microwaveSensorValue = digitalRead(MICROWAVE_SENSOR_PIN);
-  Serial.println(microwaveSensorValue);
-  delay(1000);
+
+  Serial.write("Hello World!");
+  
+  /*
+  digitalWrite(MOTOR_1A, HIGH);
+  digitalWrite(MOTOR_2A, LOW);
+  Serial.write("Hello World!");
+  delay(5000);
+  digitalWrite(MOTOR_1A, LOW);
+  digitalWrite(MOTOR_2A, HIGH);
+  delay(5000);
+
+  */
+  
 }
