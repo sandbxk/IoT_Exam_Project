0ec2d03734203170b95da02981c257627d72470f
Philip Zadeh <PhilEZad@gmail.com>
2023-03-16 11:52:44 +0100

    Logic added

--- src/main/main.ino
+++ src/main/main.ino
@@ -8,6 +8,11 @@
 #include "mqtt.hpp"
 #include "setting.hpp"
 
+
+int MOTION_SENSOR_PIN = 0;
+int MOTION_SENSOR_OUTPUT = 0; 
+int MOTION_DETECTED = 0;
+
 Setting settings = Setting
 (
   SECRET_SSID, SECRET_PASS, 
@@ -27,30 +32,26 @@ void setup()
 
   client = new IoT::Client(new Setting(settings));
   client->connect();
+
+  pinMode(MOTION_SENSOR_PIN, INPUT_PULLDOWN);
 }
 
 void loop() 
 {
+  MOTION_SENSOR_OUTPUT = digitalRead(MOTION_SENSOR_PIN);
+  Serial.println(MOTION_SENSOR_OUTPUT);
 
-  clock_t lastSensed;
-  clock_t currentTime = clock();
-
-  if (lastSensed == NULL)
+  if (MOTION_SENSOR_OUTPUT == 1 && MOTION_DETECTED == 0)
   {
-    lastSensed = clock();
-  }
-
-  double runtime_diff_ms = (currentTime - lastSensed) * 1000. / CLOCKS_PER_SEC;
-  
-
-  int microwaveSensorValue = digitalRead(MICROWAVE_SENSOR_PIN);
-
-  if (microwaveSensorValue == 1 && runtime_diff_ms > 5000) //Wait 5 seconds between sending a new MQTT message
-  {
-    client->sendMessage("open");
+    client->sendMessage("Motion Detected", "motiondetector");
+    MOTION_DETECTED = 1;
+    Serial.println("MOVEMENT DETECTED");
     delay(1000);
   }
-  
-  //client->sendMessage("Hello World!");
-  delay(1000);
+  else if (MOTION_SENSOR_OUTPUT == 0 && MOTION_DETECTED == 1)
+  {
+    MOTION_DETECTED = 0;
+    Serial.println("NO MOVEMENT");
+    delay(1000);
+  }
 }
