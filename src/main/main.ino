d7a9d686f1b8b5f3cfe0ce6850b76803b7ff5814
Rasmus Sandbæk <sandbxk@gmail.com>
2023-03-16 11:35:27 +0100

    Update diagram

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,17 +1,23 @@
 
 #define MOTOR_1A 0
 #define MOTOR_2A 14
+#define ENA 25
 
 
 // the setup routine runs once when you press reset:
 void setup() {
   pinMode(MOTOR_1A, OUTPUT);
   pinMode(MOTOR_2A, OUTPUT);
+  pinMode(ENA, OUTPUT);
+
+  digitalWrite(ENA, HIGH);
   Serial.begin(115200);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
+
+
   Serial.println("Dir1");
   digitalWrite(MOTOR_1A, HIGH);
   digitalWrite(MOTOR_2A, LOW);
@@ -25,4 +31,25 @@ void loop() {
 
 
   
+}
+
+
+void oldLoop() {
+
+  //void setup()
+  pinMode(MOTOR_1A, OUTPUT);
+  pinMode(MOTOR_2A, OUTPUT);
+  Serial.begin(115200);
+  
+
+  //L
+  Serial.println("Dir1");
+  digitalWrite(MOTOR_1A, HIGH);
+  digitalWrite(MOTOR_2A, LOW);
+  delay(1000);
+
+  Serial.println("Dir2");
+  digitalWrite(MOTOR_1A, LOW);
+  digitalWrite(MOTOR_2A, HIGH);
+  delay(1000);
 }
